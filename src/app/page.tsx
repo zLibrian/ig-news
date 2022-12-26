e6efafb834776ae73f7857b72fbfe491f69f272f
Kevin Willian <kevinwillian084@gmail.com>
2022-12-26 18:05:12 -0300

    feat: add home page with styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+import Image from 'next/image';
 import styles from './page.module.scss';
 
 export default function Home() {
-  return <main className={styles.main}>Home</main>;
+  return (
+    <main className={styles.heroContainer}>
+      <section className={styles.heroContent}>
+        <p>👏 Hey, welcome</p>
+        <h2>
+          News about <br />
+          the <span>React</span> world
+        </h2>
+        <p>
+          Get access to all the publications <br />
+          <span>for {`$9.90`} month</span>
+        </p>
+        <button type="button">Subscribe now</button>
+      </section>
+      <Image
+        src="/images/avatar.svg"
+        alt="woman coding"
+        width={340}
+        height={520}
+      />
+    </main>
+  );
 }
