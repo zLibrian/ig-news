ac10fa2bb7fcef3cb5cd8b76f5762cda4ab0fcde
Kevin Willian <kevinwillian084@gmail.com>
2022-12-28 22:32:59 -0300

    chore: salvando ṕra resetar o pc...

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,29 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { SignInButton } from '../SignInButton';
 
 import styles from './page.module.scss';
 
 export function Header() {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src="/images/logo.svg" alt="ig.news" width={100} height={100} />
         <nav>
-          <Link className={styles.active} href="/">
+          <Link className={pathname === '/' ? styles.active : ''} href="/">
             Home
           </Link>
-          <Link href="/posts">Post</Link>
+          <Link
+            className={pathname?.includes('/post') ? styles.active : ''}
+            href="/posts"
+          >
+            Post
+          </Link>
         </nav>
         <SignInButton />
       </div>
