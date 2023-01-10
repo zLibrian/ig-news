c0d7bfee3fe3f78cdab69c3d8a38037c05b653da
Kevin Willian <kevinwillian084@gmail.com>
2023-01-09 23:05:08 -0300

    feat: add handleSubscribe function to button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,14 +2,41 @@
 
 import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
+import { api } from '../../services/api/axios';
+import { getStripeJs } from '../../services/stripe-js';
 
 type SubscribeButtonProps = {
   priceId: string;
 };
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  const handleSubscription = async () => {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const { data } = await api.post('/subscribe');
+
+      const { sessionId } = data;
+
+      const stripe = await getStripeJs();
+
+      await stripe?.redirectToCheckout({ sessionId });
+    } catch (error: any) {
+      console.log(error);
+      alert('An error occurred');
+    }
+  };
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscription}>
       Subscribe now
     </button>
   );
