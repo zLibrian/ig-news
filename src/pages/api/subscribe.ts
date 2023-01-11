53c164748e12255ea9d97f2a2bea5cf182e500e7
Kevin Willian <kevinwillian084@gmail.com>
2023-01-10 23:43:00 -0300

    feat: Add verification if user already exists

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,25 +1,55 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { stripe } from '../../services/stripe';
 import { getSession } from 'next-auth/react';
+import { Fauna } from '../../services/fauna';
+import { query as q } from 'faunadb';
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 async function subscribe(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    return res.status(200).json({ ok: true });
-  }
-
   if (req.method === 'POST') {
     const session = await getSession({ req });
 
-    if (!session || !session.user) {
+    if (!session?.user) {
       return res.status(401).end('Unauthorized');
     }
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email as string,
-    });
+    const user = await Fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email as string)
+        )
+      )
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email as string,
+      });
+
+      await Fauna.query(
+        q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
 
     const checkoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       success_url: 'http://localhost:3000/posts',
       cancel_url: 'http://localhost:3000/',
       payment_method_types: ['card'],
