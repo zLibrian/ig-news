60b6f64d1170ed1193bb485b032b5dcc56b58709
Kevin Willian <kevinwillian084@gmail.com>
2023-01-13 20:35:44 -0300

    feat: add function to save a subscription into db

--- src/pages/api/webhooks/_relevantEvents.ts
+++ src/pages/api/webhooks/_relevantEvents.ts
@@ -1,11 +1,17 @@
 import Stripe from 'stripe';
+import { saveSubscription } from '../_lib/manageSubscription';
 
 export const relevantEvents = new Set(['checkout.session.completed']);
 
 export const handleStripeWebhookEvent = async (event: Stripe.Event) => {
   switch (event.type) {
     case 'checkout.session.completed':
-      console.log('Checkout session completed. EEEEEEEEEEEEEEEEE');
+      const checkoutSession = event.data.object as Stripe.Checkout.Session;
+      const subscriptionId = checkoutSession.subscription as string;
+      const customerId = checkoutSession.customer as string;
+
+      await saveSubscription({ subscriptionId, customerId });
+
       break;
     default:
       throw new Error('Unhandled event.');
