2ede555bf616b5f175b570e3f49083887ecc9485
Kevin Willian <kevinwillian084@gmail.com>
2023-01-17 20:54:40 -0300

    feat: add a new createAction variable to verify if we are creating or updating a subscription

--- src/pages/api/webhooks/_relevantEvents.ts
+++ src/pages/api/webhooks/_relevantEvents.ts
@@ -1,18 +1,37 @@
 import Stripe from 'stripe';
 import { saveSubscription } from '../_lib/manageSubscription';
 
-export const relevantEvents = new Set(['checkout.session.completed']);
+export const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
 
 export const handleStripeWebhookEvent = async (event: Stripe.Event) => {
   switch (event.type) {
+    case 'customer.subscription.updated':
+    case 'customer.subscription.deleted':
+      const subscription = event.data.object as Stripe.Subscription;
+
+      await saveSubscription({
+        subscriptionId: subscription.id,
+        customerId: subscription.customer.toString(),
+        createAction: false,
+      });
+
+      break;
     case 'checkout.session.completed':
       const checkoutSession = event.data.object as Stripe.Checkout.Session;
       const subscriptionId = checkoutSession.subscription as string;
       const customerId = checkoutSession.customer as string;
 
-      await saveSubscription({ subscriptionId, customerId });
-
+      await saveSubscription({
+        subscriptionId,
+        customerId,
+        createAction: true,
+      });
       break;
+
     default:
       throw new Error('Unhandled event.');
   }
