3694cebe5c5015e6b0d7d9251e48c34734568b08
Kevin Willian <kevinwillian084@gmail.com>
2023-01-19 21:52:02 -0300

    feat: add prismic to the app

---
+++ .slicemachine/assets/slices/Post/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Post';
+
+export default {
+  title: 'slices/Post'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Instant","spans":[]}],"description":[{"type":"paragraph","text":"Irure eu elit minim aliquip id aliqua anim. Deserunt do ea officia occaecat pariatur ipsum proident amet aliquip reprehenderit voluptate.","spans":[]}],"content":[{"type":"paragraph","text":"Ad ad consequat amet mollit ullamco culpa ea nisi velit mollit officia. In mollit quis dolore. In magna aliqua fugiat ullamco occaecat ea mollit sunt fugiat nostrud eiusmod nisi.","spans":[]}]},"id":"_Default","slice_type":"post"}} />
+_Default.storyName = ''
